a8fc6cf14d95525b3920f9d7f0acb285c71048f4
Ubuntu <ubuntu@ip-172-31-95-252.ec2.internal>
2019-10-25 12:13:12 +0000

    Added view for index

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include, reverse, re_path
 from . import urls_reset
 from .views import index, register, profile, logout, login
 
+
 urlpatterns = [
     re_path(r'^register/$', register, name='register'),
     re_path(r'^profile/$', profile, name='profile'),
--- shop/urls.py
+++ shop/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, reverse, include
 from accounts import urls as urls_accounts
+from home.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(urls_accounts)),
+    path('index/', index, name='index'),
 ]
