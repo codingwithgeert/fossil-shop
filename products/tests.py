52fc74a58c9c67c09ace911f7e9e778a3c425b80
Ubuntu <ubuntu@ip-172-31-95-252.ec2.internal>
2019-10-29 20:43:23 +0000

    Added test as the one on code institute video

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+# Test is from the code institute video #
+class ProductTests(TestCase):
+    """
+    Here we'll define the tests that we'll run against our
+    Product model
+    """
+
+    def test_str(self):
+        test_title = Product(title='A product')
+        self.assertEqual(str(test_title), 'A product')
