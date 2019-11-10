c1f0312c36748a7e9d87f6df66263b08d4b38604
Ubuntu <ubuntu@ip-172-31-95-252.ec2.internal>
2019-11-10 12:41:09 +0000

    Added media root and media url

--- shop/settings.py
+++ shop/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
