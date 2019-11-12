93fbe1ae10ddc33acf22c2d28d0e2f4d258f3455
Ubuntu <ubuntu@ip-172-31-95-252.ec2.internal>
2019-11-12 22:10:54 +0000

    changed re_path to path

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import re_path, include
+from django.urls import path, include
 from .views import all_products
 
 urlpatterns = [
-    re_path(r'^$', all_products, name='fossils'),
+    path('', all_products, name='fossils'),
 ]
